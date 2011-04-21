b26020cc4f6f65fef4e776fea24ee1960964229d
Michael Nussbaum <michaelnussbaum08@gmail.com>
2011-04-20 20:51:42 -0500

    working variable binding for one var

--- match.py
+++ match.py
@@ -1,26 +1,52 @@
 import re
 
+#TODO: lots of error checking, making sure same amounts of things, proper types
 
 class MatchKey(object):
 
     def __init__(self, pattern, sub_ins, to_binds):
-        '''string, tuple, tuple'''
+        '''string, list, list'''
         self._to_binds = to_binds
         self._subbed_in = self._normal_sub(pattern, sub_ins)
         self._indices_to_bind = [m.start() for m in \
           re.finditer('%M', self._subbed_in)]
 
-        #single tuple args become not tuples, check what we have
-        if(isinstance(self._to_binds, str) and \
-          len(self._indices_to_bind) != 1):
+        if len(self._indices_to_bind) != len(to_binds):
             raise Exception("Number of format tokens doesn't" +\
               "match format args")
 
-        if(isinstance(self._to_binds, tuple) and \
-          len(self._indices_to_bind) != len(self._to_binds)):
-            raise Exception("Number of format tokens doesn't" +\
-              "match format args")
+    def is_match(self, match_on):
+        '''string -> boolean, dict mapping bindings to values'''
+        key = self._get_key()
+        regex_match = re.match(key, match_on)
+        if hasattr(regex_match, 'group') and \
+          regex_match.group(0) == match_on:
+            match = True
+            bindings = self._bindings(match_on)
+        else:
+            match = False
+            bindings = {}
+        return match, bindings
 
+    def _bindings(self, match_on):
+        '''string -> dict mappings bindings to values'''
+        bindings = {}
+        for count, var in enumerate(self._to_binds):
+            #TODO: not getting a second binding
+            bindings[var] = self._get_next_word(match_on,
+              self._indices_to_bind[count])
+        return bindings
+
+    def _get_next_word(self, words, start_index):
+        '''string, integer -> string'''
+        total_index = None
+        print words[start_index:]
+        for index, char in enumerate(words[start_index:]):
+            total_index = index + start_index
+            if char == ' ':
+                return words[start_index:total_index]
+            elif total_index == len(words) - 1:
+                return words[start_index:total_index+1]
 
     def _normal_sub(self, pattern, sub_ins):
         '''string, tuple -> string
@@ -29,7 +55,6 @@ class MatchKey(object):
         subbed_count = 0
         subbed_in = ''
         while char_index < len(pattern):
-            print subbed_in
             char = pattern[char_index]
             try:
                 next_char = pattern[char_index+1]
@@ -38,11 +63,8 @@ class MatchKey(object):
 
             if char == '%' and next_char != 'M':
                 #preserve proper string formatting
-                if isinstance(sub_ins, tuple):
-                    to_sub = sub_ins[subbed_count]
-                else:
-                    to_sub = sub_ins
-                print to_sub
+                to_sub = sub_ins[subbed_count]
+                #TODO: change string formatting so it works with < 2.7
                 subbed_in += "{}".format(to_sub)
                 char_index += 2
                 subbed_count += 1
@@ -51,7 +73,7 @@ class MatchKey(object):
                 char_index += 1
         return subbed_in
 
-    def get_key(self):
+    def _get_key(self):
         '''void -> string
         Replaces %M in _subbed_in with regex wildcards'''
         #TODO: pull out keywords and bind them
