604c9be655183d121f5ec0130a6513289232f7a5
Michael Nussbaum <michaelnussbaum08@gmail.com>
2011-04-22 01:45:19 -0500

    more tests

--- match_tests.py
+++ match_tests.py
@@ -49,9 +49,57 @@ class MatchKeyTest(unittest.TestCase):
 
     def test_no_bindings(self):
         match_key = MatchKey('test', [], [])
-        bindings = match_key._bindings('not test')
+        bindings = match_key._bindings('test a')
         self.assertDictEqual({}, bindings)
 
+    def test_bindings(self):
+        match_key = MatchKey('test %M %M', [], ['var1', 'var2'])
+        bindings = match_key._bindings('test a b')
+        self.assertDictEqual({'var1':'a', 'var2':'b'}, bindings)
+
+    def test_get_key_no_wildcard(self):
+        match_key = MatchKey('test', [], [])
+        self.assertEquals(match_key._get_key(), 'test')
+
+    def test_get_key_with_wildcard(self):
+        match_key = MatchKey('test %M %M', [], ['var1', 'var2'])
+        self.assertEqual(match_key._get_key(), 'test [^ ]* [^ ]*')
+
+    def test_is_match_all_wrong(self):
+        match_key = MatchKey('test this', [], [])
+        result = match_key.is_match('nope na')
+        self.assertFalse(result[0])
+        self.assertDictEqual(result[1], {})
+
+    def test_is_match_partial_front_match(self):
+        match_key = MatchKey('test this', [], [])
+        result = match_key.is_match('test na')
+        self.assertFalse(result[0])
+        self.assertDictEqual(result[1], {})
+
+    def test_is_match_partial_back_match(self):
+        match_key = MatchKey('test this', [], [])
+        result = match_key.is_match('nope this')
+        self.assertFalse(result[0])
+        self.assertDictEqual(result[1], {})
+
+    def test_is_match_wilds_match(self):
+        match_key = MatchKey('test %M %M', [], ['var1', 'var2'])
+        result = match_key.is_match('test yup this')
+        self.assertTrue(result[0])
+        self.assertDictEqual(result[1], {'var1':'yup', 'var2':'this'})
+
+    def test_is_match_wild_no_match_front(self):
+        match_key = MatchKey('test %M', [], ['var1'])
+        result = match_key.is_match('almost but')
+        self.assertFalse(result[0])
+        self.assertDictEqual(result[1], {})
+
+    def test_is_match_wild_no_match_end(self):
+        match_key = MatchKey('test %M nope', [], ['var1'])
+        result = match_key.is_match('test yup almost')
+        self.assertFalse(result[0])
+        self.assertDictEqual(result[1], {})
 
 if __name__=="__main__":
     unittest.main()
